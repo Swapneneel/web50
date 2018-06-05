aa6fd8956c48dc3a811cad76699cfb9c5a21fcaf
Swapneneel <swapneneel1539@gmail.com>
2018-06-05 16:58:03 +0530

    session main

--- lectur2/sessions/application.py
+++ lectur2/sessions/application.py
@@ -8,11 +8,11 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+notes = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if seesion.get("notes") is None:
-        session["notes"] = []  #This change is to diiferntiate the each user's notes
+    session["notes"] = []  
     if request.method == "POST":
         note = request.form.get("note")
         session["notes"].append(note)
