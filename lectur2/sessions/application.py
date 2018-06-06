1bf787df6a9ed134d66d8fefdc3eeb6fdc9c5160
Swapneneel <swapneneel1539@gmail.com>
2018-06-06 16:37:54 +0530

    sessions in master

--- lectur2/sessions/application.py
+++ lectur2/sessions/application.py
@@ -4,6 +4,7 @@ from flask_session import Session  #flask_session doesn't come with flask need t
 
 app = Flask(__name__)
 
+#Not understood or explained below 3 lines
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -12,7 +13,6 @@ notes = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    session["notes"] = []  
     if request.method == "POST":
         note = request.form.get("note")
         session["notes"].append(note)
